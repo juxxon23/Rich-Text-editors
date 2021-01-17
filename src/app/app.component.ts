3049a186c5b7d5e8dfeeb9a2f164f9d858a5cc6d
DarkMatter <juxxon23@hotmail.com>
2021-01-17 04:19:32 -0500

    Angular editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-editor-test';
+
+  htmlContent: string;
+
+  show(){
+    console.log(this.htmlContent);
+  }
 }
