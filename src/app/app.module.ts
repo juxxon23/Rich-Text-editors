0730a4400607d997784c3b331fd7ff2d1cb0600a
DarkMatter <juxxon23@hotmail.com>
2021-01-21 09:12:27 -0500

    Tinymce rich text editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { EditorModule } from '@tinymce/tinymce-angular';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
